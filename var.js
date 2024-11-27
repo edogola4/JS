49e5087fab9950715e209b5d0d339bcf8c803cc1
edogola4 <edogola4@gmail.com>
2024-11-27 15:14:33 +0300

    updated

--- var.js
+++ var.js
@@ -2752,6 +2752,32 @@ Array.isArray({})
 Array.isArray(1)
 console.log(Array.isArray([]));
 
-[] instanceof Array; 
+[] instanceof Array;
 1 instanceof Array;
 console.log([] instanceof Array);
+
+
+// Insert an item into an array at a speciﬁc index
+// Simple item insertion can be done with Array.prototype.splice method:
+var myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+myArray.splice(2, 0, 'foo');
+console.log(myArray);
+
+Array.prototype.insert = function (index) {
+    this.splice.apply(this, [index, 0].concat(
+        Array.prototype.slice.call(arguments, 1)));
+    return this;
+};
+["a", "b", "c", "d"].insert(2, "X", "Y", "Z").slice(1, 6);
+console.log(["a", "b", "c", "d"].insert(2, "X", "Y", "Z").slice(1, 6));
+
+
+Array.prototype.insert = function (index) {
+    index = Math.min(index, this.length);
+    arguments.length > 1
+        && this.splice.apply(this, [index, 0].concat([].pop.call(arguments)))
+        && this.insert.apply(this, arguments);
+    return this;
+};
+["a", "b", "c", "d"].insert(2, "V", ["W", "X", "Y"], "Z").join("-");
+console.log(["a", "b", "c", "d"].insert(2, "V", ["W", "X", "Y"], "Z").join("-"));
