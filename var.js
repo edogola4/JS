bd8469f654b7fef910b4e05c4ab70f0bdc762535
edogola4 <edogola4@gmail.com>
2024-12-01 00:17:24 +0300

    updated

--- var.js
+++ var.js
@@ -3926,3 +3926,39 @@ console.log(Math.cbrt(27));
 console.log(Math.cbrt(64));
 
 console.log(Math.pow(64, 1/6));
+
+
+
+// Random with gaussian distribution
+function gaussianRandom(mean, stdev) {
+    var y2;
+    var use_last = false;
+    var y1;
+    if (use_last) {
+        y1 = y2;
+        use_last = false;
+    } else {
+        var x1, x2, w;
+        do {
+            x1 = 2.0 * Math.random() - 1.0;
+            x2 = 2.0 * Math.random() - 1.0;
+            w = x1 * x1 + x2 * x2;
+        } while (w >= 1.0);
+        w = Math.sqrt((-2.0 * Math.log(w)) / w);
+        y1 = x1 * w;
+        y2 = x2 * w;
+        use_last = true;
+    }
+
+    var retval = mean + stdev * y1;
+    if (retval > 0)
+        return retval;
+    return -retval;
+}
+
+console.log(gaussianRandom(0, 1));
+console.log(gaussianRandom(2, 2));
+console.log(gaussianRandom(-5.2, 3));
+console.log(gaussianRandom(0, -2));
+console.log(gaussianRandom(0, 0));
+console.log(gaussianRandom(1, 0.5));
